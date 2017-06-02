1e530ff4ae4a8b7b5b97af4363b87c0979cf06a9
vanezp01 <vanezp01@gmail.com>
2017-06-02 06:06:54 -0700

    ability to create posts

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,4 +2,23 @@ class PicsController < ApplicationController
 
   def index
   end
+
+  def show
+  end
+
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+  end
+
+  private
+    def pic_params
+      params.require(:pic).permit(:title, :description)
+    end
+
+
+
 end
